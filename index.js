86b8ea3ee8a8edab78496e95b00a789f8c6f4d54
Sean Ryan <sjr765@gmail.com>
2018-10-26 17:35:37 -0400

    Finished Lab

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  console.log('SESSION: ', req.session);
+  next();
+});
+
+// make sure to put this AFTER your session middleware, but BEFORE you send your response!
+app.use((req, res, next) => {
+  if (!req.session.counter) req.session.counter = 0;
+  console.log('counter', ++req.session.counter); // increment THEN log
+  next(); // needed to continue through express middleware
+});
+
 app.get('/', (req, res, next) => {
   res.send('Hello');
 });
